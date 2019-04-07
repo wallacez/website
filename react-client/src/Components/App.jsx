be77c03b17332cbed14cba85a7ce869d3ba923f3
Zach Wallace <zach.wallace@live.com>
2019-04-07 13:48:32 -0700

    Add footer component to App

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
 import NavBar from './HeaderComponent/NavBar';
+import Footer from './FooterComponent/Footer';
 
 class App extends Component {
     render() {
